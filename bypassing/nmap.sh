8c5821275c32d65e1c86f67d868de8fd19f9d3d1
Mario Inga <mario21ic@gmail.com>
2020-03-25 03:06:14 -0500

    Ready

--- bypassing/nmap.sh
+++ bypassing/nmap.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 sudo nmap --script firewall-bypass -f -T1 -S 192.168.1.8 -e enp3s0 192.168.1.11 -p80 -sV
-
+sudo nmap -S 192.168.1.8 192.168.1.10 -sV -Pn -e enp3s0
+sudo hping3 -S 192.168.1.10 -a 192.168.1.66 -c 3
+sudo iptables -t nat -A POSTROUTING -p icmp -j SNAT --to-source 192.168.1.8
